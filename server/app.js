b1950ba7e8c8df7efd4fa108bfc5d7b03e40020a
Michael Hunziker <michael.hunziker@youengineering.com>
2016-05-02 10:34:35 +0200

    Disabled unneded packages. Added port listener.

--- server/app.js
+++ server/app.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 
 var express = require('express');
 var cors = require('cors');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -16,27 +14,27 @@ mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    // we're connected!
-    console.log("Connected correctly to server");
+  // we're connected!
+  console.log("Connected correctly to server");
 });
 
 var app = express();
 app.use(cors());
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+//
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+//
 app.use('/events',eventRouter);
-
+//
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -72,4 +70,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server is listening on port ' + this.address().port);
+});
