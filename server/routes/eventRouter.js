0ea7e868c09962611cc242a21bda39e75fa5926e
Michael Hunziker <michael.hunziker@youengineering.com>
2016-05-04 10:50:48 +0200

    Rewrote code to promise style

--- server/routes/eventRouter.js
+++ server/routes/eventRouter.js
@@ -1,134 +1,155 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
 var Events = require('../models/events');
-
 var eventRouter = express.Router();
-eventRouter.use(bodyParser.json());
 
 eventRouter.route('/')
-.get(function (req, res, next) {
-    Events.find({}, function (err, event) {
-        if (err) throw err;
+  .get(function (req, res, next) {
+    Events.find({})
+      .then(function (event) {
         res.json(event);
-    });
-})
+      })
+      .catch(defaultErrorHandler.bind(null, next))
+  })
 
-.post(function (req, res, next) {
-    Events.create(req.body, function (err, event) {
-        if (err) throw err;
+  .post(function (req, res, next) {
+    Events.create(req.body)
+      .then(function (event) {
+        // FIXME: Use winston
         console.log('Event created!');
-        var id = event._id;
+        res.send(200, 'Added the event with id: ' + event._id);
+      })
+      .catch(defaultErrorHandler.bind(null, next))
+  });
 
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('Added the event with id: ' + id);
-    });
-})
-
-.delete(function (req, res, next) {
-    Events.remove({}, function (err, resp) {
-        if (err) throw err;
-        res.json(resp);
-    });
-});
+// .delete(function (req, res, next) {
+//   Events.remove({})
+//     .then(function (event) {
+//       res.json(event);
+//     })
+//     .catch(function (err) {
+//       next(err);
+//     });
+// });
 
 eventRouter.route('/:eventId')
-.get(function (req, res, next) {
-    Events.findById(req.params.eventId, function (err, event) {
-        if (err) throw err;
+  .get(function (req, res, next) {
+    Events.findById(req.params.eventId)
+      .then(function (event) {
         res.json(event);
-    });
-})
+      })
+      .catch(function (err) {
+        res.sendResponse(404);
+      });
+  })
 
-.put(function (req, res, next) {
+  .put(function (req, res, next) {
     Events.findByIdAndUpdate(req.params.eventId, {
         $set: req.body
-    }, {
+      }, {
         new: true
-    }, function (err, event) {
-        if (err) throw err;
+      })
+      .then(function (event) {
         res.json(event);
-    });
-})
+      })
+      .catch(defaultErrorHandler.bind(null, next))
+  });
 
-.delete(function (req, res, next) {
-    Events.findByIdAndRemove(req.params.eventId, function (err, resp) {        if (err) throw err;
-        res.json(resp);
-    });
-});
+// .delete(function (req, res, next) {
+//   Events.findByIdAndRemove(req.params.eventId)
+//     .then(function (event) {
+//       res.json(event);
+//     })
+//     .catch(function (err) {
+//       next(err);
+//     });
+// });
 
 eventRouter.route('/:eventId/registration')
-.get(function (req, res, next) {
-    Events.findById(req.params.eventId, function (err, event) {
-        if (err) throw err;
+  .get(function (req, res, next) {
+    Events.findById(req.params.eventId)
+      .then(function (event) {
         res.json(event.registration);
-    });
-})
+      })
+      .catch(defaultErrorHandler.bind(null, next))
+  })
 
-.post(function (req, res, next) {
-    Events.findById(req.params.eventId, function (err, event) {
-        if (err) throw err;
-        event.registration.push(req.body);
-        event.save(function (err, event) {
-            if (err) throw err;
-            console.log('Updated Registration!');
-            res.json(event);
-        });
-    });
-})
+  .post(function (req, res, next) {
 
-.delete(function (req, res, next) {
-    Events.findById(req.params.eventId, function (err, event) {
-        if (err) throw err;
-        for (var i = (event.registration.length - 1); i >= 0; i--) {
-            event.registration.id(event.registration[i]._id).remove();
-        }
-        event.save(function (err, result) {
-            if (err) throw err;
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.end('Deleted all registrations!');
-        });
-    });
-});
+    Events.findById(req.params.eventId)
+      .then(addRegistration)
+      .then(saveEvent)
+      .catch(defaultErrorHandler.bind(null, next));
+
+    function addRegistration(event) {
+      event.registration.push(req.body);
+      return event;
+    }
+
+    function saveEvent(event) {
+      return event.save();
+    }
+
+    function sendResponse(event) {
+      // FIXME: Replace all console.logs with winston logger (e.g. winston.error('message'));
+      console.log('Updated Registration!');
+      res.json(event);
+    }
+
+  });
+
+// .delete(function (req, res, next) {
+//   Events.findById(req.params.eventId, function (err, event) {
+//     if (err) next(err);
+//     for (var i = (event.registration.length - 1); i >= 0; i--) {
+//       event.registration.id(event.registration[i]._id).remove();
+//     }
+//     event.save(function (err, result) {
+//       if (err) next(err);
+//       res.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//       });
+//       res.end('Deleted all registrations!');
+//     });
+//   });
+// });
 
 eventRouter.route('/:eventId/registration/:registerId')
-.get(function (req, res, next) {
-    Events.findById(req.params.eventId, function (err, event) {
-        if (err) throw err;
+  .get(function (req, res, next) {
+    Events.findById(req.params.eventId)
+      .then(function () {
         res.json(event.registration.id(req.params.registerId));
-    });
-})
+      })
+      .catch(defaultErrorHandler.bind(null, next))
+  });
 
-.put(function (req, res, next) {
-    // We delete the existing registration and insert the updated
-    // registration as a new registration
-    Events.findById(req.params.eventId, function (err, event) {
-        if (err) throw err;
-        event.registration.id(req.params.registerId).remove();
-        event.registration.push(req.body);
-        event.save(function (err, event) {
-            if (err) throw err;
-            console.log('Updated Registrations!');
-            res.json(event);
-        });
-    });
-})
+function defaultErrorHandler(err, next) {
+  next(err);
+}
 
-.delete(function (req, res, next) {
-    Events.findById(req.params.eventId, function (err, event) {
-        event.registration.id(req.params.registerId).remove();
-        event.save(function (err, resp) {
-            if (err) throw err;
-            res.json(resp);
-        });
-    });
-});
+// .put(function (req, res, next) {
+//   // We delete the existing registration and insert the updated
+//   // registration as a new registration
+//   Events.findById(req.params.eventId, function (err, event) {
+//     if (err) next(err);
+//     event.registration.id(req.params.registerId).remove();
+//     event.registration.push(req.body);
+//     event.save(function (err, event) {
+//       if (err) next(err);
+//       console.log('Updated Registrations!');
+//       res.json(event);
+//     });
+//   });
+// })
 
+// .delete(function (req, res, next) {
+//   Events.findById(req.params.eventId, function (err, event) {
+//     event.registration.id(req.params.registerId).remove();
+//     event.save(function (err, resp) {
+//       if (err) next(err);
+//       res.json(resp);
+//     });
+//   });
+// });
 
 
 module.exports = eventRouter;
