b3e5ed220f7e0e4c6c76c98f91f744132461f524
Michael Hunziker <michael.hunziker@youengineering.com>
2016-05-04 10:50:12 +0200

    Cleaned up app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,11 @@
 var mongoose = require('mongoose');
-
 var express = require('express');
 var cors = require('cors');
-// FIXME: Remove unused packages
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var eventRouter = require('./routes/eventRouter');
 
 // FIXME: Extract db stuff to module
-var url = 'mongodb://localhost:27017/dieReise';
+var url = 'mongodb://localhost:4321/dieReise';
 mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -20,61 +15,21 @@ db.once('open', function () {
 });
 
 var app = express();
+
+// FIXME: Restrict to frontend server origins to be more secure
+// FIXME: use environment config files for origins (NODE_ENV=production node app.js)
+// app.use(cors({
+//   origin: 'http://tradukt.ch'
+// }));
 app.use(cors());
-
-
-// FIXME: Uninstall npm modules. Also remove folder "views", "public", "bin"
-//
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-//
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
+
+// Routes =========================================
 app.use('/events',eventRouter);
-//
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handlers
-
-// FIXME: Extract error handlers to module
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.header("Access-Control-Allow-Origin", "http://localhost:9000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+// Error handler ==================================
+require('./modules/errorHandler')(app);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Server is listening on port ' + this.address().port);
